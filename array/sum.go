16e42979177d1ed4e1c979275501d36efff825e3
dujinkai <11817>
2021-02-02 20:22:40 +0800

    array done

--- array/sum.go
+++ array/sum.go
@@ -9,9 +9,19 @@ func Sum(numbers []int) (sum int) {
 
 func SumAll(numbersToSum ...[]int) (sums []int) {
 
-	sums = make([]int, len(numbersToSum))
-	for i, numbers := range numbersToSum {
-		sums[i] = Sum(numbers)
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+	return
+}
+
+func SumAllTails(numbersToTrail ...[]int) (sumtrail []int) {
+	for _, numbers := range numbersToTrail {
+		if len(numbers) != 0 {
+			sumtrail = append(sumtrail, Sum(numbers[1:]))
+		} else {
+			sumtrail = append(sumtrail, 0)
+		}
 	}
 	return
 }
--- array/sum_test.go
+++ array/sum_test.go
@@ -6,11 +6,13 @@ import (
 )
 
 func assertCorrentMessage(t testing.TB, got, want int) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %d, want %d", got, want)
 	}
 }
 func assertCorrentMessageSlice(t testing.TB, got, want []int) {
+	t.Helper()
 	if !reflect.DeepEqual(got, want) {
 		t.Errorf("got %d, want %d", got, want)
 	}
@@ -42,6 +44,21 @@ func TestSumAll(t *testing.T) {
 	t.Run("single slice", func(t *testing.T) {
 		got := SumAll([]int{1, 1, 1})
 		want := []int{3}
+		assertCorrentMessageSlice(t, got, want)
+	})
+
+}
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("make the sums of some slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2}, []int{0, 9})
+		want := []int{2, 9}
+		assertCorrentMessageSlice(t, got, want)
+	})
+
+	t.Run("safely sum empty slices", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{3, 4, 5})
+		want := []int{0, 9}
 		assertCorrentMessageSlice(t, got, want)
 	})
 
